6286a2e47e25bb75dcc01495a16e9ed835b356b2
Prasant Koirala <koiralaprashanta@gmail.com>
2026-07-23 16:32:58 +0000

    feat: updated the redoply script to used docker compose

--- redoply-site.sh
+++ redoply-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd ~/Personal_PE_fellowship_portfolio
 git fetch && git reset origin/main --hard
-python -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
